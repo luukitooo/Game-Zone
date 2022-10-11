df81a830f946df1065760bda4845d31aabca8be0
luka <luka.janjgava.1@btu.edu.ge>
2022-10-11 21:45:50 +0400

    LifecycleOwner extension added

---
+++ app/src/main/java/com/lukaarmen/gamezone/common/extentions/LifecycleOwnerExtensions.kt
@@ -0,0 +1,32 @@
+package com.lukaarmen.gamezone.common.extentions
+
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
+
+fun LifecycleOwner.doInBackground(
+    context: CoroutineContext = EmptyCoroutineContext,
+    action: suspend (CoroutineScope) -> Unit
+) : Job {
+    return when (this) {
+        is Fragment -> {
+            viewLifecycleOwner.lifecycleScope.launch(context) {
+                repeatOnLifecycle(Lifecycle.State.STARTED) {
+                    action(this@launch)
+                }
+            }
+        }
+        else -> {
+            lifecycleScope.launch {
+                action(this@launch)
+            }
+        }
+    }
+}
