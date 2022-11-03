acc7cd714d4b3937154e85c44f635b808ff44171
luka <luka.janjgava.1@btu.edu.ge>
2022-11-04 01:17:12 +0400

    users searching added

--- app/src/main/java/com/lukaarmen/gamezone/ui/tabs/chat/chatfragment/ChatFragmentFragment.kt
+++ app/src/main/java/com/lukaarmen/gamezone/ui/tabs/chat/chatfragment/ChatFragmentFragment.kt
@@ -1,12 +1,16 @@
 package com.lukaarmen.gamezone.ui.tabs.chat.chatfragment
 
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.tabs.TabLayoutMediator
 import com.lukaarmen.gamezone.R
 import com.lukaarmen.gamezone.common.base.BaseFragment
 import com.lukaarmen.gamezone.common.extentions.doInBackground
+import com.lukaarmen.gamezone.common.extentions.hide
+import com.lukaarmen.gamezone.common.extentions.show
 import com.lukaarmen.gamezone.databinding.FragmentChatBinding
+import com.lukaarmen.gamezone.model.User
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -15,6 +19,10 @@ class ChatFragmentFragment : BaseFragment<FragmentChatBinding>(FragmentChatBindi
     private val viewModel: ChatViewModel by viewModels()
 
     private val userPagerAdapter = UserPagerAdapter()
+
+    private var isSearching = false
+    private var currentAllUsersList = emptyList<User>()
+    private var currentSavedUsersList = emptyList<User>()
 
     override fun init() {
         binding.viewPager.adapter = userPagerAdapter
@@ -36,19 +44,53 @@ class ChatFragmentFragment : BaseFragment<FragmentChatBinding>(FragmentChatBindi
                 )
             )
         }
+        binding.btnSearch.setOnClickListener {
+            isSearching = !isSearching
+            toggleSearch(isSearching)
+        }
+        binding.etSearch.doOnTextChanged { text, start, before, _ ->
+            if (start != 0 || before != 0) {
+                searchForUsers(text.toString())
+            }
+        }
     }
 
     override fun observers() {
         doInBackground {
             viewModel.allUsersFlow.collect { users ->
                 userPagerAdapter.setAllUsers(users)
+                currentAllUsersList = users
             }
         }
         doInBackground {
             viewModel.savedUsersFlow.collect { users ->
                 userPagerAdapter.setSavedUsers(users)
+                currentSavedUsersList = users
             }
         }
+    }
+
+    private fun toggleSearch(isSearching: Boolean) {
+        if (isSearching) {
+            binding.btnSearch.setImageResource(R.drawable.ic_cross)
+            binding.etSearch.show()
+        } else {
+            binding.btnSearch.setImageResource(R.drawable.ic_search)
+            binding.etSearch.hide()
+        }
+    }
+
+    private fun searchForUsers(username: String) {
+        userPagerAdapter.setAllUsers(
+            currentAllUsersList.filter { user ->
+                user.username?.contains(username) ?: false
+            }
+        )
+        userPagerAdapter.setSavedUsers(
+            currentSavedUsersList.filter { user ->
+                user.username?.contains(username) ?: false
+            }
+        )
     }
 
 }
